c7400279f4fca94d955a980b8ad5a18b9e74c8bf
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2022-12-05 21:34:50 -0500

    Eight ball working

---
+++ src/EightBall.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { answers } from './Eightball_answers';
+import './EightBall.css';
+
+const EightBall = (props) => {
+	const genRandom = () => Math.floor(Math.random() * props.answers.length);
+
+	// const getAnswer = () => {
+	// 	setAnswer(genRandom());
+	// };
+
+	const setColor = () => {
+		if (props.answers[answer].color === 'red') {
+			return 'EightBall-red-button';
+		} else if (props.answers[answer].color === 'green') {
+			return 'EightBall-green-button';
+		} else {
+			return 'EightBall-goldenrod-button';
+		}
+	};
+
+	const [ answer, setAnswer ] = useState(null);
+	console.log(answer);
+
+	return (
+		<div className="EightBall">
+			<button
+				className={answer === null ? 'EightBall-starter-button' : setColor()}
+				onClick={() => setAnswer(genRandom())}
+			>
+				{answer === null ? 'Think of a Question' : answers[answer].msg}
+			</button>
+		</div>
+	);
+};
+
+EightBall.defaultProps = {
+	answers
+};
+export default EightBall;
